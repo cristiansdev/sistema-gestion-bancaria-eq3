1b46703327b089b215b24ad7fddbddb4d0a8ce28
cristiansdev <cristian192003@outlook.es>
2026-02-16 22:57:53 -0600

    feat: initial structure for BankCore microservices with docker-compose

---
+++ ms-accounts/src/main/java/com/bankcore/accounts/MsAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.bankcore.accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAccountsApplication.class, args);
+	}
+
+}
